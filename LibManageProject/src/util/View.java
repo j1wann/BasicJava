8cefc33764c7c2fa85960598b05113c4322428e8
PC-14 <PC-14@DESKTOP-SEIF0SH>
2024-03-16 18:01:17 +0900

    SS

---
+++ LibManageProject/src/util/View.java
@@ -0,0 +1,47 @@
+package util;
+
+public enum View {
+	HOME, //메인화면
+	NOTICE,	//공지사항
+	BOOK_SEARCH,	//도서검색
+	ROOM,	//스터디룸
+	LOGIN,
+	
+	MEM_LOGIN,
+	AD_LOGIN,
+	MEM_SIGN,
+	
+	MEM_HOME,
+	MY_PAGE,	//마이페이지
+	
+	AD_HOME,
+	BOOK_RETURN,	//반납
+	BOOK_RENT,	//대여
+	MANAGE,	//관리
+	
+	ROOM_RES,
+	
+	INFO,
+	PASS_UPDATE,
+	TEL_UPDATE,
+	ADDR_UPDATE,
+	NAME_UPDATE,
+	
+	RENT_HISTORY,
+	RENT_UPDATE,
+	MY_REVIEW,
+	
+	
+	
+	
+	//리뷰관련
+	REVIEW_INSERT,
+	REVIEW_DETAIL,
+	REVIEW_DELETE,
+	REVIEW_UPDATE
+	
+	
+	;
+	
+	
+}
